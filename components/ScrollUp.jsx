99c76e64b2e1465c3dda080360e85ceed928a2df
MStackPro <naszymanasseh@gmail.com>
2024-12-08 22:23:54 +0100

    added scroll-to-top

---
+++ components/ScrollUp.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import ScrollToTop from "react-scroll-to-top";
+import { IoIosArrowUp } from "react-icons/io";
+
+export default function ScrollUp() {
+  return (
+    <main>
+      <ScrollToTop
+        className="flex justify-center items-center"
+        style={{ backgroundColor: "black", width: "2.5rem", height: "2.5rem" }}
+        smooth
+        component={<IoIosArrowUp className="text-primary text-3xl" />}
+      />
+    </main>
+  );
+}
